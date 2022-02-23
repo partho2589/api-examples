41e4123913cf8b633337201a62db75d28e512ca8
partho2589 <parthomalo615@.com>
2022-02-24 03:45:06 +0600

    country

--- js/countries.js
+++ js/countries.js
@@ -18,13 +18,33 @@ const countryDiv = countries  => {
 
        const div = document.createElement('div');
        div.classList.add('country')
-       h3.innerText = country.name.common;
-       div.appendChild(h3)
-       const p = document.createElement('p')
-       p.innerText = country.name.capital;
-       div.appendChild (p)
+       div.innerHTML = `
+       <h3>${country.name.common}</h3>
+       <p>${country.capital}
+       <button onclick="loadCountryByName('${country.name.common}')">Details </button>
+       
+       `
+    //    h3.innerText = country.name.common;
+    //    div.appendChild(h3)
+    //    const p = document.createElement('p')
+    //    p.innerText = country.capital;
+    //    div.appendChild (p)
        countriesDiv.appendChild(div)
     })
-   
-    
+}
+
+const loadCountryByName = name => {
+    const url = `https://restcountries.com/v3.1/name/${name}`
+    fetch(url)
+    .then(res => res.json())
+    .then(data => displayCountryDetail(data[0]))
+}
+
+const displayCountryDetail = country  => {
+    console.log(country)
+    const  countryDiv = document.getElementById('country-detail')
+    countryDiv.innerHTML = `
+    <h5> ${country.name.common}</h5>
+    img src ="${country.population}"
+    `
 }
