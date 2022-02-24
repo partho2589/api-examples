fe0ec03bfb9c52f91badb2f97bf5f35e83268801
partho2589 <parthomalo615@.com>
2022-02-24 12:35:07 +0600

    mealbd

---
+++ js/mealkdb.js
@@ -0,0 +1,35 @@
+const searchFood = () => {
+    const searchFild = document.getElementById('search-field');
+    const searchText = searchFild.value
+    // console.log(searchText)
+    searchFild.value = ' ';
+
+    const url = `https://www.themealdb.com/api/json/v1/1/search.php?s=${searchText}`
+    // console.log(url)
+    fetch(url)
+    .then(res => res.json())
+    .then(data => displaySearchResults(data.meals))
+};
+
+const displaySearchResults = meals => {
+    const searchResult = document.getElementById('search-result')
+    meals.forEach(meal => {
+        const div = document.createElement('div')
+        div.classList.add('clo')
+        div.innerHTML = `
+        <div class="card h-100">
+            <img src="${meal.strMealThumb}" class="card-img-top" alt="...">
+            <div class="card-body">
+              <h5 class="card-title">${meal.strMeal}</h5>
+              <p class="card-text">
+              ${meal.strInstructions.slice(0, 250)}
+              </p>
+            </div>
+        </div>
+        
+        `
+        searchResult.appendChild(div)
+
+    })
+    
+}
