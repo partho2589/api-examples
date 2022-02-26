f4ff72cd7a53c8b560b8105468781d13f08e0c79
partho2589 <parthomalo615@.com>
2022-02-26 12:26:47 +0600

    function added

--- js/mealdb.js
+++ js/mealdb.js
@@ -2,12 +2,9 @@
 const searchFood = () => {
     const searchFild = document.getElementById('search-field');
     const searchText = searchFild.value
-   
-    // console.log(searchText)
     searchFild.value = ' ';
 
     const url = `https://www.themealdb.com/api/json/v1/1/search.php?s=${searchText}`
-    // console.log(url)
     fetch(url)
     .then(res => res.json())
     .then(data => displaySearchResults(data.meals))
@@ -16,7 +13,6 @@ const searchFood = () => {
 const displaySearchResults = meals => {
     const searchResult = document.getElementById('search-result')
     meals.forEach(meal => {
-        console.log(meal)
         const div = document.createElement('div')
         div.classList.add('clo')
         div.innerHTML = `
@@ -39,8 +35,7 @@ const displaySearchResults = meals => {
 
 const loadMealDetail = mealId => {
     const url = `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${mealId}`
-    // console.log(url)
-    fetch(url)
+      fetch(url)
     .then(res => res.json())
     .then(data => displayMealDetail(data.meals[0]))
 }
